d8fc0325e584ef4bfdf80e3ce2978031079e16c5
diabetes-tool <eskilsimonkannewadsholt@gmail.com>
2020-12-30 04:26:25 +0100

    add input panels

--- static/js/plusbtn.js
+++ static/js/plusbtn.js
@@ -1,9 +1,11 @@
-const $bottomBar = $('.bottom-bar')
+const $bottomBar = $('.bottom-bar');
+const $inputPanel = $('.input-panel');
 
 $('.btn-plus').on('click', function(e) {
   if ($bottomBar.hasClass('activated')) {
     $('#overlay').removeClass('active');
     $bottomBar.removeClass('activated');
+    $inputPanel.removeClass('activated');
   }
   else {
     $('#overlay').addClass('active');
--- static/js/selectTool.js
+++ static/js/selectTool.js
@@ -1,9 +1,42 @@
 $('.tool').on('click', function(e) {
   $('.tool').removeClass('selected');
   $(this).addClass('selected');
+  $('.input-panel').addClass('activated');
+  openInputPanel();
   e.stopPropagation();
 });
 
 $('body').on('click', function(e) {
   $('.tool').removeClass('selected');
 });
+
+function openInputPanel() {
+  if ($('.blood').hasClass('selected')) {
+    openBGInput();
+  }
+  else if ($('.insulin').hasClass('selected')) {
+    openInsulinInput();
+  }
+  else if ($('.meal').hasClass('selected')) {
+    openMealInput();
+  }
+  else if ($('.note').hasClass('selected')) {
+    openNoteInput();
+  }
+}
+
+function openBGInput() {
+  $inputPanel.html("<h1 class='center'>Enter BG</h1>");
+}
+
+function openInsulinInput() {
+  $inputPanel.html("<h1 class='center'>Enter Bolus</h1>");
+}
+
+function openMealInput() {
+  $inputPanel.html("<h1 class='center'>Enter Meal</h1>");
+}
+
+function openNoteInput() {
+  $inputPanel.html("<h1 class='center'>Enter Note</h1>");
+}
